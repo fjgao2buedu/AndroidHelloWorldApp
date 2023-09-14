7ff3805666aeb79516bc8a3a8084ae05000d2d33
fjgao <fjgao@bu.edu>
2023-09-14 16:54:17 -0400

    using string resource with placeholder fixed problems

--- app/src/main/java/com/example/helloworld/MainActivity.kt
+++ app/src/main/java/com/example/helloworld/MainActivity.kt
@@ -10,8 +10,8 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val textView: TextView? = findViewById(R.id.activity_main_textview)//https://developer.android.com/guide/topics/resources/layout-resource#idvalue
-        var myName: String = "THIS IS A NAME"//intentional, as this is in a public repo
-        textView?.setText("Hi, my name is $myName")//https://developer.android.com/reference/android/widget/TextView#setText(java.lang.CharSequence)
+        val myName = "THIS IS A NAME"//intentional, as this is in a public repo
+        textView?.text = getString(R.string.say_hello, myName)//https://developer.android.com/guide/topics/resources/string-resource#formatting-strings
 
     }
 }
