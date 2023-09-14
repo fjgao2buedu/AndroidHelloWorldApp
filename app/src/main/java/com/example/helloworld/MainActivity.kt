0f06c2587bfbb6de121a1a058557aa5607be11f5
fjgao <fjgao@bu.edu>
2023-09-14 16:33:28 -0400

    display var myName

--- app/src/main/java/com/example/helloworld/MainActivity.kt
+++ app/src/main/java/com/example/helloworld/MainActivity.kt
@@ -2,10 +2,16 @@ package com.example.helloworld
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val textView: TextView? = findViewById(R.id.activity_main_textview)//https://developer.android.com/guide/topics/resources/layout-resource#idvalue
+        var myName: String = "THIS IS A NAME"//intentional, as this is in a public repo
+        textView?.setText("Hi, my name is $myName")//https://developer.android.com/reference/android/widget/TextView#setText(java.lang.CharSequence)
+
     }
 }
